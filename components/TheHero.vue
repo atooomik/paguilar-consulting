8369b536459f31996216a6d62acdaba78fcc2ef4
Isacc <atoomik.dd@hotmail.com>
2020-11-05 09:55:24 -0600

    adding a basic hero

---
+++ components/TheHero.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="w-full flex flex-col lg:flex-row bg-ui-bgContrast">
+    <div class="px-4">
+      <h1 class="text-xl font-semibold">
+        Consultora especializada en el ramo inmobiliario
+      </h1>
+      <p>
+        Nostrodum exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequeant.
+      </p>
+    </div>
+    <div class="w-full h-72 overflow-hidden">
+      <img
+        class="w-full h-full object-cover"
+        src="~/assets/images/smiling-businesswoman.jpg"
+        alt="Mujer asesorando a una pareja"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TheHero',
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,6 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">h-consulting</h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+  <div>
+    <the-hero />
   </div>
 </template>
 
