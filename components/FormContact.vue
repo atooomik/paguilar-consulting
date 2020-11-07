e33d19667035c554920d553c28ac55ef09f23025
Isacc <atoomik.dd@hotmail.com>
2020-11-07 01:02:56 -0600

    ready FormContact

---
+++ components/FormContact.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="p-4 mx-auto border border-ui-outline rounded-lg lg:max-w-md">
+    <form action="">
+      <label class="block mb-6">
+        <span class="text-gray-700">Nombre</span>
+        <input
+          class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-ux-blue"
+          id="inline-full-name"
+          type="text"
+          required
+        />
+      </label>
+
+      <label class="block mb-6">
+        <span class="text-gray-700">Celular</span>
+        <input
+          class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-ux-blue"
+          id="inline-full-name"
+          type="text"
+          required
+        />
+      </label>
+
+      <label class="block mb-6">
+        <span class="text-gray-700">Correo electrónico</span>
+        <input
+          class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-ux-blue"
+          id="inline-full-name"
+          type="text"
+          required
+        />
+      </label>
+
+      <label class="block mb-6">
+        <span class="text-gray-700">Servicio de tu interes:</span>
+        <select
+          class="form-select bg-gray-200 mt-1 block w-full focus:border-ux-blue"
+          required
+        >
+          <option>Asesoria hipotecaria</option>
+          <option>Trámite de escrituras</option>
+          <option>Regularización de escrituras</option>
+          <option>Otros</option>
+        </select>
+      </label>
+
+      <label class="flex items-center mb-6">
+        <input type="checkbox" class="form-checkbox" />
+        <span class="ml-2"
+          >He leido y acepto el
+          <span class="underline">aviso de privacidad.</span></span
+        >
+      </label>
+      <div class="flex">
+        <button class="btn btn--blue mx-auto">Enviar</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FormContact',
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -97,11 +97,19 @@
       </div>
     </div>
     <div class="section-container max-w-7xl mx-auto">
-      <h2 class="pb-8 text-h3 lg:text-h2 text-center">Ultimas entradas</h2>
+      <h2 class="pb-8 text-h3 text-center lg:text-h2">Ultimas entradas</h2>
       <div class="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
         <card-blog-resume :largeCard="false" />
         <card-blog-resume :largeCard="false" />
         <card-blog-resume :largeCard="false" />
+      </div>
+    </div>
+    <div class="section-container max-w-7xl mx-auto">
+      <h2 class="pb-8 text-h3 text-center lg:text-h2">
+        Permiteme aportar mi experiencia a tú proyecto
+      </h2>
+      <div>
+        <form-contact />
       </div>
     </div>
   </div>
