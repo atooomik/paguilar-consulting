ea5f45ceec4e871da0b0269c78bf2c6d3a9bb6aa
Isacc Nuñez <isacc.nunez@apithy.com>
2021-01-25 22:22:39 -0600

    hotfix fontawesome icons

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,7 +23,7 @@ export default {
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
-    { src: '~/plugins/font-awesome.js', ssr: false },
+    { src: '~/plugins/font-awesome.js', ssr: true },
     { src: '~/plugins/vue-scroll.js', ssr: false}
     ],
 
